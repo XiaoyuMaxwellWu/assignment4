e36bfe6259f8f421fedda6146cf20aa3c4177a42
Max Wu <maxwellwu1997@gmail.com>
2022-07-06 11:48:56 -0700

    finish first task

--- frontend/src/components/SocialSidebar/PlayersList.tsx
+++ frontend/src/components/SocialSidebar/PlayersList.tsx
@@ -1,4 +1,5 @@
-import { Heading, StackDivider, VStack } from '@chakra-ui/react';
+import { Heading, StackDivider, Tooltip, VStack } from '@chakra-ui/react';
+import { ListItem } from '@material-ui/core';
 import React from 'react';
 import useCoveyAppState from '../../hooks/useCoveyAppState';
 import usePlayersInTown from '../../hooks/usePlayersInTown';
@@ -17,23 +18,31 @@ import PlayerName from './PlayerName';
  *
  */
 export default function PlayersInTownList(): JSX.Element {
-  const { currentTownFriendlyName } = useCoveyAppState();
+  const { currentTownFriendlyName, currentTownID } = useCoveyAppState();
   const players = usePlayersInTown();
-  players.sort((p1, p2) =>
-    p1.userName.localeCompare(p2.userName, undefined, { numeric: true, sensitivity: 'base' }),
-  );
+  const copyOfArrayPassedToComponent = players.concat([]);
+  copyOfArrayPassedToComponent.sort((p1, p2) =>
+  p1.userName.localeCompare(p2.userName, undefined, { numeric: true, sensitivity: 'base' }),
+);
+  const label = `Town ID: ${currentTownID}`;
   return (
     <VStack
       align='left'
       height='100%'
       divider={<StackDivider borderColor='gray.200' />}
       borderRadius='4px'>
-      <Heading fontSize='xl' as='h2'>
+      <Tooltip label= {label}>
+        <Heading fontSize='xl' as='h2'>
         Current town: {currentTownFriendlyName}
       </Heading>
-      {players.map(p => (
-        <PlayerName key={p.id} player={p} />
+      </Tooltip>
+      <ol>
+      {copyOfArrayPassedToComponent.map(p => (
+        <ListItem key={p.id}>        
+          <PlayerName player={p} />
+        </ListItem>
       ))}
+      </ol>
     </VStack>
   );
 }
